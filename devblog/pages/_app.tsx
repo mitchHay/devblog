7432071bf1afbdc68d94478e2b3edb194c838325
mitchHay <mitchell.hayward97@gmail.com>
2023-05-28 08:39:59 +1000

    Preconnect to lordicon

--- devblog/pages/_app.tsx
+++ devblog/pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
+        <link rel="preconnect" href="https://cdn.lordicon.com" />
       </Head>
       <style jsx global>{`
         html,
