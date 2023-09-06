1964c3f349a105828cd4b8dc67f74e376476680b
mitchHay <mitchell.hayward97@gmail.com>
2023-09-06 22:20:23 +1000

    Update to astro 3

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,15 +6,12 @@ const {
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  experimental: {
-    assets: true
-  },
+  scopedStyleStrategy: 'where',
   image: {
     service: sharpImageService()
   },
